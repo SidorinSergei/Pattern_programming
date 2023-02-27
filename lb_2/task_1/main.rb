7364402ba58532f7c36e67f85082cf9f9cb111d3
SidorinSergei <95975705+SidorinSergei@users.noreply.github.com>
2023-02-27 22:02:58 +0300

    all items of task 1 have been completed

--- lb_2/task_1/main.rb
+++ lb_2/task_1/main.rb
@@ -1,1 +1,5 @@
 require_relative 'student.rb'
+
+check1 = Student.new({last_name:'Денисов',first_name:'Вячеслав',patronymic:'Олегович',id:'1',email:'denisov007@yandex.ru',phone:'+79892270793',git:'https://github.com/slavadenisov',telegram:'@den'})
+check2= Student.new({last_name:'Дзюба',first_name:'Артём',patronymic:'Сергеевич',email:'dzuyba_loko007@yandex.ru',git:'https://github.com/dzu',telegram:'@dzuyba07'})
+check3= Student.new({last_name:'Жирков',first_name:'Юрий',patronymic:'Валентинович'})
--- lb_2/task_1/student.rb
+++ lb_2/task_1/student.rb
@@ -1,7 +1,8 @@
 class Student
   attr_accessor :id, :last_name, :first_name, :patronymic, :phone, :telegram, :email, :git
 
-  PHONE = /^@((8|\+7)[\-]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}&/
+  #PHONE = /^@((8|\+7)[\-]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}&/
+  PHONE= /\A\+?\d{11}\z/.freeze
   EMAIL= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   TELEGRAM=/^@[A-Za-z\d_]{5,32}$/
   GIT=/\Ahttps:\/\/github\.com\/\w+\z/
@@ -10,15 +11,15 @@ class Student
     value.match?(EMAIL)
   end
 
-  def Student.correct_git(value)
+  def Student.correct_git?(value)
     value.match?(GIT)
   end
 
-  def Student.correct_phone(value)
+  def Student.correct_phone?(value)
     value.match?(PHONE)
   end
 
-  def Student.correct_telegram(value)
+  def Student.correct_telegram?(value)
     value.match?(value)
   end
 
@@ -31,9 +32,38 @@ class Student
     raise ArgumentError,'ФИО - обязательные параметры' unless
       @last_name&&@first_name&&@patronymic
 
+    set_contacts(arg[:phone],arg[:telegram],arg[:email],arg[:git])
+
+    validate
+  end
+
+  def get_info
+    puts "\nФамилия - #{last_name}\nИмя - #{first_name}\nОтчество - #{patronymic}\nID - #{id}\nНомер - #{phone}\nTelegram - #{telegram}\nПочта - #{email}\nGit - #{git}"
+  end
+
+  def set_contacts(phone,telegram,email,git)
+    raise ArgumentError,'Неверный формат номера телефона' if phone&&!Student.correct_phone?(phone)
+    raise ArgumentError,'Неверный формат имени пользователя в telegram' if telegram&&!Student.correct_telegram?(telegram)
+    raise ArgumentError,'Неверный формат электронной почты' if email&&!Student.correct_email?(email)
+    raise ArgumentError, 'Неверный формат ссылки на git' if git&&!Student.correct_git?(git)
     @phone=phone
     @telegram=telegram
     @email=email
     @git=git
+
   end
+
+  def valid_git
+    raise ArgumentError,'Git обязательно должен быть указан!' unless git
+  end
+
+  def valid_contact
+    raise ArgumentError,'Должен быть указан как минимум один контакт' unless phone||telegram||email
+  end
+
+  def validate
+    valid_contact
+    valid_git
+  end
+
 end
