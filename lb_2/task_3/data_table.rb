935323fe3350f628a2a2ac3ce44cc426d444eac8
Sergei <super.sidorin2017@yandex.ru>
2023-03-20 23:34:05 +0300

    point 2

--- lb_2/task_3/data_table.rb
+++ lb_2/task_3/data_table.rb
@@ -4,4 +4,16 @@ class Data_table
   def initialize(table)
     @table = table
   end
+
+  def get_element(row, col)
+    @table[row][col]
+  end
+
+  def num_columns
+    @table[0].size
+  end
+
+  def num_rows
+    @table.size
+  end
 end
