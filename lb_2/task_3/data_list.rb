746ce85302166f42147b3f5f4a69cf9447209c9c
Sergei <super.sidorin2017@yandex.ru>
2023-03-20 23:54:25 +0300

    point 4

--- lb_2/task_3/data_list.rb
+++ lb_2/task_3/data_list.rb
@@ -1,6 +1,23 @@
 class Data_list
   def initialize(source_array)
+    source_array.sort!
     @arr = source_array
+    @selected = []
   end
 
+  def select(number)
+    @selected << number
+  end
+
+  def get_selected
+    @selected
+  end
+
+  def get_names
+
+  end
+
+  def get_data
+
+  end
 end
