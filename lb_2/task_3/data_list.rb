3a61a99471134b78acbd49acafc383d69df61a33
Sergei <super.sidorin2017@yandex.ru>
2023-03-28 00:27:06 +0300

    Update data_list.rb

--- lb_2/task_3/data_list.rb
+++ lb_2/task_3/data_list.rb
@@ -6,11 +6,18 @@ class DataList
   end
 
   def select(number)
-    @selected << number
+    begin
+      numbers.is_a?(Array) ? @select += numbers.map! { |el| Integer(el - 1) } : @select += [Integer(numbers - 1)]
+    rescue ArgumentError
+      puts "Переданное(-ые) значение(-ия) должно(-ы) быть целочисленным(и)!"
+    end
+    nil
   end
 
   def get_selected
-    @selected
+    res = @select.inject([]) { |acc, el| acc.push @arr[el].id  }
+    @select = []
+    res
   end
 
   def get_names
